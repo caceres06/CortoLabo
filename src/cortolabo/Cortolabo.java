f24cd71982b49abbdb3ca12daa61503d10756207
caceres06 <37302560+caceres06@users.noreply.github.com>
2018-06-20 13:15:43 -0600

    Modificacion y se agrego clases

--- src/cortolabo/Cortolabo.java
+++ src/cortolabo/Cortolabo.java
@@ -5,12 +5,15 @@
  */
 package cortolabo;
 
+import java.awt.Container;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -20,7 +23,7 @@ public class Cortolabo {
 
     public JLabel lblnombre, lblanio,lbldirector,lblpais, lblclasificacion,lblexistencia;
     public JTextField nombre, anio, director,pais;
-    public JComboBox clasificaion;
+    public JComboBox clasificacion;
     
     ButtonGroup existencia = new ButtonGroup();
     public JRadioButton si;
@@ -28,6 +31,33 @@ public class Cortolabo {
     
     public JButton Agregar, Borrar, Actualizar, Buscar;
     
+    private static final int ANCHOC = 160, ALTOC=40;
+    
+    DefaultTableModel tm;
+    
+    public Cortolabo(){
+       super("Cine");
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setLayout(null);
+       agregarLabels();
+       proyeccion();
+       llenarTabla();
+       Container container = getContetPane();
+       container.add(lblnombre);
+       container.add(lblanio);
+       container.add(lbldirector);
+       container.add(lblpais);
+       container.add(lblclasificacion);
+       container.add(lblexistencia);
+       container.add(nombre);
+       container.add(anio);
+       container.add(director);
+       container.add(pais);
+       container.add(clasificacion);
+       container.add(existencia);
+       container.add(lblnombre);
+       
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
